96c6e260a3cdd3b3eda69d639006d83bb754c93a
Htue Ko <htooko1101@gmail.com>
2021-07-23 00:30:51 +0630

    add di for local database

---
+++ app/src/main/java/com/htueko/simpletodo/common/data/di/DatabaseModule.kt
@@ -0,0 +1,34 @@
+package com.htueko.simpletodo.common.data.di
+
+import android.content.Context
+import androidx.room.Room
+import com.htueko.simpletodo.common.data.local.LocalConstant
+import com.htueko.simpletodo.common.data.local.database.TodoDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DatabaseModule {
+
+    // to provide database
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): TodoDatabase = Room.databaseBuilder(
+        context,
+        TodoDatabase::class.java,
+        LocalConstant.DB_NAME
+    ).build()
+
+    // to provide dao
+    @Singleton
+    @Provides
+    fun provideTodoDao(db: TodoDatabase) = db.getTodoDao()
+
+}
