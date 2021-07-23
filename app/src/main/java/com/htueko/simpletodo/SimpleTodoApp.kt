d1a208b5220bbbcea184e953b5b999046b578fe5
Htue Ko <htooko1101@gmail.com>
2021-07-23 22:57:39 +0630

    add application class and setup logging

---
+++ app/src/main/java/com/htueko/simpletodo/SimpleTodoApp.kt
@@ -0,0 +1,18 @@
+package com.htueko.simpletodo
+
+import android.app.Application
+import com.htueko.simpletodo.common.util.Logger
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SimpleTodoApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initLogger()
+    }
+
+    private fun initLogger() {
+        Logger.init()
+    }
+
+}
